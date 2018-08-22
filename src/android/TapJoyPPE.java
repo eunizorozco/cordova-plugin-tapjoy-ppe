472e89613a9024eb6e025e93ad79f2e14e6a136e
Eunice Orozco <eorozco+lms@katanatechworks.com>
2018-08-22 20:06:48 +0800

    update plugin.xml

--- src/android/TapJoyPPE.java
+++ src/android/TapJoyPPE.java
@@ -1,19 +1,53 @@
-package com.eorozco.plugin;
+package org.apache.cordova.plugin;
+
+import android.util.Log;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import com.tapjoy.TJConnectListener;
+import com.tapjoy.Tapjoy;
+import com.tapjoy.TapjoyConnectFlag;
+
+import java.util.Hashtable;
+
 public class TapJoyPPE extends CordovaPlugin {
 
+    public static final String TAG = "TapJoyPPE";
+
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    protected void pluginInitialize() {
+        super.pluginIntialize()
+
+        Tapjoy.setDebugEnabled(true);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("connect")) {
+
+            Hashtable<String, Object> connectFlags = new Hashtable<String, Object>();
+            connectFlags.put(TapjoyConnectFlag.ENABLE_LOGGING, "true");
 
             String key = data.getString(0);
             String message = "SDK Key, " + key;
             callbackContext.success(message);
+
+            Tapjoy.connect(this, key, connectFlags, new TJConnectListener() {
+                @Override
+                public void onConnectSuccess() {
+                    Log.e(TAG, "Tapjoy connect call succeed");
+                    callbackContext.success("Success");
+                }
+
+                @Override
+                public void onConnectFailure() {
+                    Log.e(TAG, "Tapjoy connect call failed");
+                    callbackContext.error("Error");
+                }
+            });
 
             return true;
 
